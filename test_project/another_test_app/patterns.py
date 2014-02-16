3bf9e463bd7dcecfad47a1f7381298f110ec0c32
Keryn Knight <keryn@kerynknight.com>
2014-02-16 11:08:37 +0000

    Demonstrate nested callables work.

--- test_project/another_test_app/patterns.py
+++ test_project/another_test_app/patterns.py
@@ -16,3 +16,25 @@ def example_header(request):
     """
     return render_to_string('example_header.html', {},
                             context_instance=RequestContext(request))
+
+
+def nested_callable2(request):
+    return render_to_string('example_nested.html', {},
+                            context_instance=RequestContext(request))
+
+
+def nested_callable1(request):
+    return nested_callable2
+
+
+@is_pattern(assets={'top': ('css/example_base.css', 'css/example_progress.css')})
+def example_nested_callable(request):
+    """
+    Somtimes it is useful to be able to have your pattern serve as a dispatch
+    method for another callable. When rendering, the ``Pattern`` object will
+    continue trying to call return values until they are no longer callable.
+
+    This allows potentially infinite nesting of callables, as long as they
+    take the required parameters (``request``).
+    """
+    return nested_callable1
